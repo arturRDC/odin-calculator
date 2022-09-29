bfa02f3c0b02a1d20e6e413b655013b44411beb4
Artur Ribeiro da Cunha <artur.rdc1@gmail.com>
2022-09-29 00:42:57 -0300

    Add initial styling

---
+++ app.js
@@ -0,0 +1,23 @@
+// Adds two numbers
+function add(a, b) {
+    return a + b;
+}
+
+// Subtracts two numbers
+function subtract(a, b) {
+    return a - b;
+}
+
+// Multiplies two numbers
+function multiply(a, b) {
+    return a * b;
+}
+
+// Divides two numbers
+function divide(a, b) {
+    return a / b;
+}
+
+function operate(operator, a, b) {
+    operator(a,b);
+}
